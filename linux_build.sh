50e31c661c2c5db8d9de808efd4426f3755f8472
Nikita Yankovskii <yannik98@yandex.ru>
2022-07-06 17:36:00 +0300

    Добавлен скрипт для загрузки зависимостей на линукс

---
+++ linux_build.sh
@@ -0,0 +1,7 @@
+sudo apt install portaudio19-dev
+sudo apt install flac lame opus-tools
+sudo apt-get install --no-install-recommends build-essential python3-pip python3-setuptools python3-wheel libspeechd-dev
+pip3 install -r requirements.txt 
+pip3 install scons lxml
+pip3 install rhvoice-wrapper-bin
+pip3 install torch
